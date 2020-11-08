79d32f48dc1fd3418656bb7eac3dab137ae3337c
Sean Morgan <insideseanshead@gmail.com>
2020-11-07 23:27:19 -0800

    added jumbotron for hero and navbar

---
+++ src/components/Hero/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./styles.css";
+
+export default function Hero() {
+  return (
+    <div className="jumbotron jumbotron-fluid">
+      <div className="container">
+        <h1 className="display-4">Sean Morgan</h1>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/NavBar/index.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <button
+        className="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNavDropdown"
+        aria-controls="navbarNavDropdown"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">
+              Portfolio <span className="sr-only">(current)</span>
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              Info/Contact
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="http://www.github.com/insideseanshead" target="_blank">
+              GitHub
+            </a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">
+              LinkdIn
+            </a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+}
