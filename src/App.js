f682b7b9665063e5a6638d645cd11c0c09fef579
Sean Morgan <insideseanshead@gmail.com>
2020-11-09 20:15:10 -0800

    got routing and links to work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
 import './App.css';
 import Hero from "./components/Hero";
-import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Portfolio from "./pages/portfolio"
+import Contact from "./pages/contact"
+import NavBar from "./components/NavBar"
 
 function App() {
   return (
     <div className="App">
       <Hero />
-      <NavBar />
+      
+      <div className="navBar">
+        
+        <Router>
+        <NavBar />
+        <Route exact path="/" component={Portfolio} />
+        <Route exact path="/contact" component={Contact} />
+        </Router>
+        
+      </div>
     </div>
+    
   );
 }
 
--- src/components/NavBar/index.js
+++ src/components/NavBar/index.js
@@ -1,43 +1,54 @@
 import React from "react";
+import { Link } from "react-router-dom"
 
 export default function NavBar() {
   return (
-    <nav className="navbar navbar-expand-lg navbar-light bg-light">
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNavDropdown"
-        aria-controls="navbarNavDropdown"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className="nav-item active">
-            <a className="nav-link" href="#">
-              Portfolio <span className="sr-only">(current)</span>
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              Info/Contact
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="http://www.github.com/insideseanshead" target="_blank">
-              GitHub
-            </a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">
-              LinkdIn
-            </a>
-          </li>
-        </ul>
-      </div>
-    </nav>
+    
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-toggle="collapse"
+          data-target="#navbarNavDropdown"
+          aria-controls="navbarNavDropdown"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarNavDropdown">
+          <ul className="navbar-nav">
+            <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Portfolio
+            </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="/contact" className="nav-link">
+                Info/Contact
+              </Link>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="http://www.github.com/insideseanshead"
+                target="_blank"
+              >
+                GitHub
+              </a>
+            </li>
+            <li className="nav-item">
+              <a
+                className="nav-link"
+                href="https://www.linkedin.com/in/sean-morgan-1398b4a1/"
+                target="_blank"
+              >
+                LinkedIn
+              </a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+  
   );
 }
