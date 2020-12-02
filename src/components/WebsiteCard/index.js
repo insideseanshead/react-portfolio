a0336eba483be5028eddb1b068ec8060f3cea920
Sean Morgan <insideseanshead@gmail.com>
2020-12-01 19:34:06 -0800

    testing heroku img url

--- src/components/WebsiteCard/index.js
+++ src/components/WebsiteCard/index.js
@@ -26,18 +26,18 @@ export default function WebsiteCard(props) {
 
 
       <div className="card float-left m-2">
-        <img src={sites[0].image} className="card-img-top" alt={sites[0].alt} />
+        <img src={sites[1].image} className="card-img-top" alt={sites[1].alt} />
         <div className="card-body">
-          <h5 className="card-title">{sites[0].title}</h5>
-          <p className="card-text">{sites[0].description}</p>
+          <h5 className="card-title">{sites[1].title}</h5>
+          <p className="card-text">{sites[1].description}</p>
           <a
-            href={sites[0].depolyed}
+            href={sites[1].depolyed}
             className="btn btn-primary mr-3"
             target="_blank"
           >
             Deployed Site
           </a>
-          <a href={sites[0].github} className="btn btn-primary" target="_blank">
+          <a href={sites[1].github} className="btn btn-primary" target="_blank">
             Github
           </a>
         </div>
