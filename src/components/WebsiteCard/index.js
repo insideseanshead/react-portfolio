5b836d7ea271bd12e2b9fae8385329cbf13ca3fb
Sean Morgan <insideseanshead@gmail.com>
2021-01-26 22:41:52 -0800

    used .map to dynamically create cards

--- src/components/WebsiteCard/index.js
+++ src/components/WebsiteCard/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 import sites from "../../sites.json";
 import wondir from "../../images/wondir.png"
 
-export default function WebsiteCard(prop) {
+export default function WebsiteCard() {
   return (
     <div className="row justify-content-center">
       <div className="card float-left m-2">
@@ -23,88 +23,6 @@ export default function WebsiteCard(prop) {
           </a>
         </div>
       </div>
-      
-      <div className="card float-left m-2">
-        <img src={sites[1].image} className="card-img-top" alt={sites[1].alt} />
-        <div className="card-body">
-          <h5 className="card-title">{sites[1].title}</h5>
-          <p className="card-text">{sites[1].description}</p>
-          <a
-            href={sites[1].deployed}
-            className="btn btn-primary mr-3"
-            target="_blank"
-          >
-            Deployed Site
-          </a>
-          <a href={sites[1].github} className="btn btn-primary" target="_blank">
-            Github
-          </a>
-        </div>
-      </div>
-
-      <div className="card float-left m-2">
-        <img src={sites[2].image} className="card-img-top" alt={sites[2].alt} />
-        <div className="card-body">
-          <h5 className="card-title">{sites[2].title}</h5>
-          <p className="card-text">{sites[2].description}</p>
-          <a
-            href={sites[2].deployed}
-            className="btn btn-primary mr-3"
-            target="_blank"
-          >
-            Deployed Site
-          </a>
-          <a href={sites[2].github} className="btn btn-primary" target="_blank">
-            Github
-          </a>
-        </div>
-      </div>
-
-      <div className="card float-left m-2">
-        <img src={sites[3].image} className="card-img-top" alt={sites[3].alt} />
-        <div className="card-body">
-          <h5 className="card-title">{sites[3].title}</h5>
-          <p className="card-text">{sites[3].description}</p>
-          <a
-            href={sites[3].deployed}
-            className="btn btn-primary mr-3"
-            target="_blank"
-          >
-            Deployed Site
-          </a>
-          <a href={sites[3].github} className="btn btn-primary" target="_blank">
-            Github
-          </a>
-        </div>
-      </div>
-
-      <div className="card float-left m-2">
-        <img src={sites[4].image} className="card-img-top" alt={sites[4].alt} />
-        <div className="card-body">
-          <h5 className="card-title">{sites[4].title}</h5>
-          <p className="card-text">{sites[4].description}</p>
-          <a href={sites[4].deployed} className="btn btn-primary mr-3" target="_blank">
-            Deployed Site
-          </a>
-          <a href={sites[4].github} className="btn btn-primary" target="_blank">
-            Github
-          </a>
-        </div>
-      </div>
-
-      <div className="card float-left m-2">
-        <img src={sites[5].image} className="card-img-top" alt={sites[5].alt} />
-        <div className="card-body">
-          <h5 className="card-title">{sites[5].title}</h5>
-          <p className="card-text">{sites[5].description}</p>
-         
-          <a href={sites[5].github} className="btn btn-primary" target="_blank">
-            Github
-          </a>
-        </div>
-      </div>
-
-      
     </div>
   );
 }
--- src/pages/portfolio/index.js
+++ src/pages/portfolio/index.js
@@ -1,22 +1,85 @@
 import React from "react";
 import "./styles.css";
-import gerome from "../../images/gerome.jpg"
-import makeMyDate from "../../images/makeMyDate.png"
-import directory from "../../images/directory.png"
-import readMe from "../../images/readMe.png"
-import schedule from "../../images/schedule.png"
-import quiz from "../../images/quiz.png"
-import wondir from "../../images/wondir.png"
 import WebsiteCard from "../../components/WebsiteCard";
-import sites from "../../sites.json";
 
+const sites = [
+  {
+      "id": 1,
+      "image": "https://sean-morgan-portfolio.herokuapp.com/static/media/wondir.a6357d9d.png",
+      "alt": "Wondir Logo",
+      "title": "Wondir",
+      "description": "NPC Generator for Dungeons and Dragons.",
+      "deployed": "https://wandir-front.herokuapp.com/",
+      "github": "https://github.com/asmarcd/wandir-front-end"
+  },
+  {
+      "id": 2,
+      "image": "https://sean-morgan-portfolio.herokuapp.com/static/media/gerome.e66bc659.jpg",
+      "alt": "Geomore the Gnome Logo",
+      "title": "Gerome the Gnome",
+      "description": "NPC Generator for Dungeons and Dragons.",
+      "deployed": "https://gerome-the-gnome.herokuapp.com/",
+      "github": "https://github.com/insideseanshead/gerome-the-gnome"
+  },
+  {
+      "id": 3,
+      "image": "https://sean-morgan-portfolio.herokuapp.com/static/media/makeMyDate.e7cab47d.png",
+      "alt": "Make My Date Logo",
+      "title": "Make my Date",
+      "description": "Dating app that selects random night out you and that special someone.",
+      "deployed": "https://mfallesen.github.io/plannerly/",
+      "github": "https://github.com/mfallesen/plannerly"
+  },
+  {
+      "id": 4,
+      "image": "https://sean-morgan-portfolio.herokuapp.com/static/media/directory.528d43ed.png",
+      "alt": "Employee directory screen shot",
+      "title": "Employee Directory",
+      "description": "A directory that builds itself using an API database and can be sorted and searched through.",
+      "deployed": "https://sean-employee-directory.herokuapp.com/",
+      "github": "https://github.com/insideseanshead/employee-directory"
+  },
+  {
+      "id": 5,
+      "image": "https://sean-morgan-portfolio.herokuapp.com/static/media/schedule.cdf786e2.png",
+      "alt": "Day planner screen shot",
+      "title": "Day Planner",
+      "description": "A scheduling app that color codes time of day and when user has something scheduled.",
+      "deployed": "https://insideseanshead.github.io/Day-Planner/",
+      "github": "https://github.com/insideseanshead/Day-Planner"
+  },
+  {
+      "id": 6,
+      "image": "https://sean-morgan-portfolio.herokuapp.com/static/media/readMe.73137051.png",
+      "alt": "ReadMe generator screen shot",
+      "title": "ReadMe Generator",
+      "description": "A node application to help developers write readMe documents.",
+      "github": "https://github.com/insideseanshead/readmeGenerator"
+  }
+]
 
 export default function index() {
   return (
     <div className="container mt-3 mb-5">
-      
-      <WebsiteCard />
-      
+      {sites.map((site)=>(
+        <div className="card float-left m-2">
+        <img src={site.image} className="card-img-top" alt={site.alt} />
+        <div className="card-body">
+          <h5 className="card-title">{site.title}</h5>
+          <p className="card-text">{site.description}</p>
+          <a
+            href={site.deployed}
+            className="btn btn-primary mr-3"
+            target="_blank"
+          >
+            Deployed Site
+          </a>
+          <a href={site.github} className="btn btn-primary" target="_blank">
+            Github
+          </a>
+        </div>
+      </div>
+      ))}
     </div>
   );
 }
